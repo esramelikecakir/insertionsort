c661c3cbf8904c8e67bb2973d5eefe866c8c643c
esramelikecakir <77747702+esramelikecakir@users.noreply.github.com>
2022-07-11 18:09:40 +0300

    insertion

---
+++ main.py
@@ -0,0 +1,7 @@
+#[22,27,16,2,18,6] we use Insertion Sort
+#Fırstly find the smallest number and swap each other.
+#2 is the smallest so our new lis t --> [2,27,16,22,18,6]
+#Find other smallest number except 2 and replace
+#6 is this number so --> [2,6,16,22,18,27]
+#Countiune like that --> [2,6,16,22,18,27]
+#[2,6,16,18,22,27] And we got the solution.
